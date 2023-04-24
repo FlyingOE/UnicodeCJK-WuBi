3e7e70d97706de73d10810006e32e0c8b91c4ed9
Freddie Wu <freddie.wu@capitaledge.cn>
2023-04-24 09:50:33 +0800

    add combined CJK list in Unicode order

---
+++ combine.sh
@@ -0,0 +1,15 @@
+(  cat CJKRadicalsSupplement.txt                #  U+2E80 ~  U+2EFF
+   cat KangxiRadicals.txt                       #  U+2F00 ~  U+2FDF
+   cat CJKStrokes.txt                           #  U+31C0 ~  U+31EF
+   cat CJK-A.txt                                #  U+3400 ~  U+4DBF
+   cat CJK.txt                                  #  U+4E00 ~  U+9FFF
+   cat CJKCompatibilityIdeographs.txt           #  U+F900 ~  U+FAFF
+   cat CJK-B.txt                                # U+20000 ~ U+2A6DF
+   cat CJK-C.txt                                # U+2A700 ~ U+2B73F
+   cat CJK-D.txt                                # U+2B740 ~ U+2B81F
+   cat CJK-E.txt                                # U+2B820 ~ U+2CEAF
+   cat CJK-F.txt                                # U+2CEB0 ~ U+2EBEF
+   cat CJKCompatibilityIdeographsSupplement.txt # U+2F800 ~ U+2FA1F
+   cat CJK-G.txt                                # U+30000 ~ U+3134F
+   cat CJK-H.txt                                # U+31350 ~ U+323AF
+) > CJK_all.txt
